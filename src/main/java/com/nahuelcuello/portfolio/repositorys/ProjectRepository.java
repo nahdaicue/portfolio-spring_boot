8afa22db5dcf67553196d91d25fc32887492b61a
nahdaicue <nahdaicue@gmail.com>
2025-12-29 18:43:57 -0300

    add the repository annotation

--- src/main/java/com/nahuelcuello/portfolio/repositorys/ProjectRepository.java
+++ src/main/java/com/nahuelcuello/portfolio/repositorys/ProjectRepository.java
@@ -3,8 +3,9 @@ package com.nahuelcuello.portfolio.repositorys;
 
 import com.nahuelcuello.portfolio.entitys.Projects;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ProjectRepository extends JpaRepository <Projects, Long>{
     
 }
--- src/main/java/com/nahuelcuello/portfolio/repositorys/UserRepository.java
+++ src/main/java/com/nahuelcuello/portfolio/repositorys/UserRepository.java
@@ -3,9 +3,9 @@ package com.nahuelcuello.portfolio.repositorys;
 
 import com.nahuelcuello.portfolio.entitys.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository <User, Long>{
-    
-    
     
 }
